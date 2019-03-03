c0c6f35ec45c986496a2a97a095adb90c7cd0efd
Ajith Kumar <45239303+AjithKumarChennai@users.noreply.github.com>
2019-03-03 20:07:20 +0530

    Create Stetho.java

---
+++ Stetho.java
@@ -0,0 +1,15 @@
+implementation 'com.facebook.stetho:stetho:1.5.0'
+implementation 'com.uphyca:stetho_realm:2.3.0'
+
+repositories {
+    maven {
+        url 'https://github.com/uPhyca/stetho-realm/raw/master/maven-repo'
+    }
+}
+
+-----
+
+maven { url 'https://github.com/WickeDev/stetho-realm/raw/master/maven-repo' }
+
+
+----
